3516d40e1ae5c5cee1c2af40b8eb1542cfdb260d
Wison Francisco <wilsonfranciscogamboa@yandex.ru>
2026-06-27 13:18:09 +0300

    feat: Analise exploratoria e seleçao de recursos para o churn

--- src/ml_churn/modeling/train/modeling.py
+++ src/ml_churn/modeling/train/modeling.py
@@ -1,7 +1,7 @@
 import os 
 import pandas as pd
 import sqlalchemy
-from sqlalchemy import text # 1. Importe a função text
+
 
 TRAIN_DIR = os.path.dirname(os.path.abspath(__file__))
 MODELING_DIR = os.path.dirname(TRAIN_DIR)       # src/
@@ -12,7 +12,15 @@ DATA_DIR = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'data')
 engine = sqlalchemy.create_engine("sqlite:///" + os.path.join(DATA_DIR, 'booking_db.sqlite'))
 
 
-
+# tabela ABT para todas as analises
 abt = pd.read_sql_table('tb_abt_churn', engine)
 
-print(abt.head(10))
+
+df_oot = abt[abt['ref_date'] == abt['ref_date'].max()].copy() # filtrando base out of time
+
+df_abt = abt[abt['ref_date'] < abt['ref_date'].max()].copy() # filtrando base ABT
+
+print('tabela out of time: ', df_oot.shape)
+print(df_oot['target_churn'].mean())
+print('tabela ABT: ', df_abt.shape)
+print(df_abt['target_churn'].mean())
