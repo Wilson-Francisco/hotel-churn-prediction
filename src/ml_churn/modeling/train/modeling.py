5dad017a103ae3dc79b4c25ad014ec616ba20d8f
Wison Francisco <wilsonfranciscogamboa@yandex.ru>
2026-06-27 14:34:32 +0300

    Criar modelo ml de Classificador de Árvore de Decisão

--- src/ml_churn/modeling/train/modeling.py
+++ src/ml_churn/modeling/train/modeling.py
@@ -1,6 +1,18 @@
 import os 
 import pandas as pd
 import sqlalchemy
+import numpy as np
+import matplotlib.pyplot as plt
+import nltk
+from imblearn.over_sampling import SMOTE
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import pandas as pd
+from feature_engine.encoding import OneHotEncoder
+from sklearn import metrics
+from imblearn.pipeline import Pipeline
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 
 
 TRAIN_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -20,7 +32,147 @@ df_oot = abt[abt['ref_date'] == abt['ref_date'].max()].copy() # filtrando base o
 
 df_abt = abt[abt['ref_date'] < abt['ref_date'].max()].copy() # filtrando base ABT
 
-print('tabela out of time: ', df_oot.shape)
-print(df_oot['target_churn'].mean())
-print('tabela ABT: ', df_abt.shape)
-print(df_abt['target_churn'].mean())
+df_abt.drop(['ref_date', 'review_id', 'user_id'],axis=1, inplace=True )
+
+
+# Converte a coluna review_date para datetime
+df_abt['review_date'] = pd.to_datetime(df_abt['review_date'], format='%Y-%m-%d')
+
+
+# Baixa o dicionário de regras do VADER (focado em avaliações/sentimentos)
+# nltk.download('vader_lexicon')
+sia = SentimentIntensityAnalyzer()
+
+
+# Tratamento dos nulos coluna review_text
+df_abt['review_text'] = df_abt['review_text'].fillna("")
+
+# TRATAMENTO E EXTRAÇÃO DE SENTIMENTO
+def extrair_sentimento(texto):
+    if str(texto).strip() == "":
+        return 0.0 # Texto vazio recebe score neutro
+    
+    scores = sia.polarity_scores(str(texto))
+    return scores['compound'] # Retorna a nota geral de sentimento (-1 a +1)
+
+print("Extraindo o sentimento dos textos...")
+# Aplica a função em toda a base abt
+df_abt['sentimento_review'] = df_abt['review_text'].apply(extrair_sentimento)
+
+# As features numéricas e categóricas selecionadas
+features_numericas = [
+    'sat_media_score_overall', 'sentimento_review', 'sat_media_score_limpeza',
+    'sat_media_score_conforto', 'sat_media_score_comodidades', 'freq_total_reviews'
+]
+
+features_categoricas = ['hotel_name', 'user_gender', 'age_group', 'traveller_type']
+
+# Concatenacao das features
+features = features_numericas + features_categoricas
+
+
+# Cria o encoder e aplicar OneHotEncoder# Cria o encoder e aplicar OneHotEncoder
+onehot = OneHotEncoder(variables=features_categoricas)
+
+
+# Normalizar as variáveis
+norm = MinMaxScaler()
+
+
+# Balaceamento dos dados
+smote = SMOTE(random_state=42)
+
+target = 'target_churn'
+
+# Dividir os dados em treino e teste para iniciar a fase de criação do modelo
+X_train, X_test, y_train, y_test = train_test_split(df_abt[features], df_abt[target] , test_size = 0.2, random_state = 42)
+
+
+# Modelo de árvore de Classificador de Árvore de Decisão
+clf_tree = tree.DecisionTreeClassifier(max_depth=5, random_state = 42)
+
+# Pipeline com todos objetos
+model_pipeline = Pipeline(steps = [("onehot", onehot),
+                                   ("norm ", norm ),
+                                   ("smote", smote),
+                                   ("clf_tree", clf_tree)])
+
+
+""" 
+# Ajustando o modelo
+model_pipeline.fit(X_train[features], y_train)
+
+# Salvando o algoritmo
+model = pd.Series(
+    {
+        "model": model_pipeline,
+        "features": features
+    } )
+
+
+# Métricas de treino do modelo
+pred_train = model["model"].predict(X_train[features])
+pred_proba_train = model["model"].predict_proba(X_train[features])[:,1]
+
+# Calcular acuracia do modelo de treino
+scores_train = model["model"].score(X_train[features], y_train)
+print(f"Acurácia em Treinamento: {scores_train}")
+
+# Calcular curva ROC do modelo de treino
+scores_roc_auc_train = metrics.roc_auc_score(y_train, pred_proba_train)
+print(f"Curva ROC em Treinamento: {scores_roc_auc_train}")
+
+# Métricas de teste do modelo
+pred_test = model["model"].predict(X_test[features])
+pred_proba_test = model["model"].predict_proba(X_test[features])[:,1]
+
+# Calcular acuracia do modelo de teste
+scores_test = model["model"].score(X_test[features], y_test)
+print(f"Acurácia em Test: {scores_test}")
+
+# Calcular curva ROC do modelo de teste
+scores_auc_test = metrics.roc_auc_score(y_test, pred_proba_test)
+print(f"Curva ROC em Test:{scores_auc_test}")
+
+# Treino da Curva ROC
+roc_curve_train = metrics.roc_curve(y_train, pred_proba_train)
+
+# Teste da Curva ROC
+roc_curve_test = metrics.roc_curve(y_test, pred_proba_test)
+
+# Gráfico da curva ROC
+plt.plot(roc_curve_train[0], roc_curve_train[1])
+plt.plot(roc_curve_test[0], roc_curve_test[1])
+plt.grid(True)
+plt.plot([0,1],[0,1], "--", color="black")
+plt.title("Curva ROC")
+plt.ylabel("Sensibilidade")
+plt.xlabel("1 - Especificidade")
+plt.legend(
+    [
+        f"Treino: {100*scores_train:.2f}%",
+        f"Teste: {100*scores_test:.2f}%"
+    ])
+
+plt.show() """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
